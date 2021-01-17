09e772878c9520f94bb6654a8084db9f5f5d0897
Slava Bacherikov <slava@bacher09.org>
2021-01-17 22:02:50 +0200

    Update makepackage.sh

--- makepackage.sh
+++ makepackage.sh
@@ -13,9 +13,9 @@ rsync -rv --exclude=.git --exclude=.tmp --exclude=textures --exclude=*.tga \
      --exclude=*.yml --exclude=.tx --exclude=Makefile --exclude=*.sp2 --exclude=*.mid \
      --exclude=README.md --exclude=cmake --exclude=CMakeLists.txt --exclude=*.sh  \
      --exclude=Dockerfile --exclude=docker \
-     --exclude=*.sh --exclude=*.dem "$1" /tmp/build-data
+     --exclude=*.sh --exclude=*.sw[op] --exclude=*.dem "$1" /tmp/build-data
 
 
 pushd /tmp/build-data
-7za a -tzip -mx=9  "/artifacts/xonotic-data-$(date +%Y%m%d).pk3"  ./
+7za a -tzip -mx=9  "/var/tmp/artifacts/xonotic-data-$(date +%Y%m%d).pk3"  ./
 popd
